679f4927334df71222a26637e40b67801d5a8671
Iyadi Cyril <iyadicy@gmail.com>
2021-08-31 16:02:53 +0100

    Create portfolio_load.js
    add javascript array containing details for all cards
    
    add function for loading the contents dynamically to the empty project cards containers

---
+++ scripts/portfolio_load.js
@@ -0,0 +1,277 @@
+const projectCards = [
+  {
+    id: 'desktop-project-1',
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    image_top: {
+      url: 'Images/project-image-1.jpg',
+      alt: '',
+    },
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    image_bottom: {
+      url: '#',
+      alt: '',
+    },
+    button: {
+      text: 'See this project',
+      image: {
+        url: 'svg/ic_arrowright.svg',
+        alt: '',
+      },
+    },
+    hasImageBottom: false,
+    linktolive: 'https://see-why.github.io/MyPortofolio/',
+    linktosource: 'https://github.com/see-why/MyPortofolio',
+  },
+  {
+    id: 'desktop-project-2',
+    name: 'Project name goes...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    image_top: {
+      url: 'Images/project-image-2.jpg',
+      alt: '',
+    },
+    technologies: ['HTML/CSS', 'Ruby on Rails', '+1'],
+    image_bottom: {
+      url: '#',
+      alt: '',
+    },
+    button: {
+      text: 'See this project',
+      image: {
+        url: 'svg/ic_arrowright.svg',
+        alt: '',
+      },
+    },
+    hasImageBottom: true,
+    linktolive: 'https://see-why.github.io/MyPortofolio/',
+    linktosource: 'https://github.com/see-why/MyPortofolio',
+  },
+  {
+    id: 'mobile-project-1',
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    image_top: {
+      url: 'Images/project-image-1.jpg',
+      alt: '',
+    },
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    image_bottom: {
+      url: '#',
+      alt: '',
+    },
+    button: {
+      text: 'See this project',
+      image: {
+        url: 'svg/ic_arrowright.svg',
+        alt: '',
+      },
+    },
+    hasImageBottom: false,
+    linktolive: 'https://see-why.github.io/MyPortofolio/',
+    linktosource: 'https://github.com/see-why/MyPortofolio',
+  },
+  {
+    id: 'mobile-project-2',
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    image_top: {
+      url: 'Images/project-image-2.jpg',
+      alt: '',
+    },
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    image_bottom: {
+      url: '#',
+      alt: '',
+    },
+    button: {
+      text: 'See this project',
+      image: {
+        url: 'svg/ic_arrowright.svg',
+        alt: '',
+      },
+    },
+    hasImageBottom: false,
+    linktolive: 'https://see-why.github.io/MyPortofolio/',
+    linktosource: 'https://github.com/see-why/MyPortofolio',
+  },
+  {
+    id: 'mobile-project-3',
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    image_top: {
+      url: 'Images/project-image-3.jpg',
+      alt: '',
+    },
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    image_bottom: {
+      url: '#',
+      alt: '',
+    },
+    button: {
+      text: 'See this project',
+      image: {
+        url: 'svg/ic_arrowright.svg',
+        alt: '',
+      },
+    },
+    hasImageBottom: false,
+    linktolive: 'https://see-why.github.io/MyPortofolio/',
+    linktosource: 'https://github.com/see-why/MyPortofolio',
+  },
+  {
+    id: 'mobile-project-4',
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    image_top: {
+      url: 'Images/project-image-4.jpg',
+      alt: '',
+    },
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    image_bottom: {
+      url: '#',
+      alt: '',
+    },
+    button: {
+      text: 'See this project',
+      image: {
+        url: 'svg/ic_arrowright.svg',
+        alt: '',
+      },
+    },
+    hasImageBottom: false,
+    linktolive: 'https://see-why.github.io/MyPortofolio/',
+    linktosource: 'https://github.com/see-why/MyPortofolio',
+  },
+  {
+    id: 'mobile-project-5',
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    image_top: {
+      url: 'Images/project-image-5.jpg',
+      alt: '',
+    },
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    image_bottom: {
+      url: '#',
+      alt: '',
+    },
+    button: {
+      text: 'See this project',
+      image: {
+        url: 'svg/ic_arrowright.svg',
+        alt: '',
+      },
+    },
+    hasImageBottom: false,
+    linktolive: 'https://see-why.github.io/MyPortofolio/',
+    linktosource: 'https://github.com/see-why/MyPortofolio',
+  },
+  {
+    id: 'mobile-project-6',
+    name: 'Project name goes here',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+    image_top: {
+      url: 'Images/project-image-6.jpg',
+      alt: '',
+    },
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    image_bottom: {
+      url: '#',
+      alt: '',
+    },
+    button: {
+      text: 'See this project',
+      image: {
+        url: 'svg/ic_arrowright.svg',
+        alt: '',
+      },
+    },
+    hasImageBottom: false,
+    linktolive: 'https://see-why.github.io/MyPortofolio/',
+    linktosource: 'https://github.com/see-why/MyPortofolio',
+  },
+];
+
+function loadProjects() {
+  projectCards.forEach((each) => {
+    const parentDiv = document.getElementById(each.id);
+
+    const figure = document.createElement('figure');
+
+    if (each.id !== 'desktop-project-1') {
+      figure.className = 'wrapper';
+    }
+
+    const imagetop = document.createElement('img');
+
+    imagetop.src = each.image_top.url;
+    imagetop.alt = each.image_top.url;
+    imagetop.className = 'blockImage';
+
+    figure.appendChild(imagetop);
+
+    const gridDiv = document.createElement('div');
+    gridDiv.className = 'grid-container';
+
+    if (each.id === 'desktop-project-2') {
+      gridDiv.className = 'grid-container aside';
+    }
+
+    const h3 = document.createElement('h3');
+    h3.innerText = each.name;
+
+    gridDiv.appendChild(h3);
+
+    const innerDiv = document.createElement('div');
+    innerDiv.className = 'code-buttons';
+
+    if (each.id === 'desktop-project-1') {
+      innerDiv.className = 'code-buttons d-flex';
+    }
+    const flex = 'd-flex';
+    const ul = document.createElement('ul');
+    ul.className = flex;
+
+    each.technologies.forEach((each) => {
+      const li = document.createElement('li');
+      li.innerText = each;
+      li.className = flex;
+      ul.appendChild(li);
+    });
+
+    innerDiv.appendChild(ul);
+    gridDiv.appendChild(innerDiv);
+
+    const button = document.createElement('button');
+    button.innerText = each.button.text;
+    button.className = 'arrow-click d-flex';
+
+    const buttonImage = document.createElement('img');
+    buttonImage.className = 'display-first';
+    buttonImage.src = each.button.image.url;
+    buttonImage.alt = each.button.image.alt;
+
+    button.appendChild(buttonImage);
+
+    gridDiv.appendChild(button);
+
+    parentDiv.appendChild(figure);
+    parentDiv.appendChild(gridDiv);
+
+    if (each.hasImageBottom) {
+      const imagebottom = document.createElement('img');
+      imagebottom.src = each.image_bottom.url;
+      imagebottom.alt = each.image_bottom.alt;
+
+      const figureBottom = document.createElement('figure');
+      figureBottom.className = 'grid-container-bottom';
+      figureBottom.appendChild(imagebottom);
+
+      parentDiv.appendChild(figureBottom);
+    }
+  });
+}
+
+const section = document.getElementById('');
+section.addEventListener('click', loadProjects);
